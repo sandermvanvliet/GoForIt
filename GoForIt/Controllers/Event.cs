95aa64749fcee7674a1ae5b18ebad389b47165ad
sandermvanvliet <sander@codenizer.nl>
2016-02-05 10:43:07 +0100

    Added event REST API

---
+++ GoForIt/Controllers/Event.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using GoForIt.Models;
+
+namespace GoForIt.Controllers
+{
+    public class Event
+    {
+        public string Application { get; set; }
+        public DateTime Timestamp { get; set; }
+        public List<ParameterModel> Parameters { get; set; }
+    }
+}
---
+++ GoForIt/Controllers/EventController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace GoForIt.Controllers
+{
+    public class EventController : ApiController
+    {
+        // POST api/values
+        public void Post([FromBody] Event value)
+        {
+            var path = Path.Combine(ConfigurationManager.AppSettings["eventStore"],
+                DateTime.UtcNow.ToString("yyyyMMddHHmmss") + ".json");
+
+            File.WriteAllText(path, JsonConvert.SerializeObject(value));
+        }
+    }
+}
